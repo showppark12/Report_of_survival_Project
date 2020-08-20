477cb9edfad71fb64d11d19e49cb62947b0adf4e
showppark12 <showppark12@naver.com>
2020-08-21 02:17:23 +0900

    create report

--- report/models.py
+++ report/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
 from account.models import Account
 
 class Report(models.Model):
     user = models.ForeignKey(Account, related_name='survivor', on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    message = models.charField(max_length=100, blank=True)
+    pub_date = models.DateTimeField(default=timezone.now)
+    message = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+            return self.user.name
 
     class Meta:
         db_table ='report'
