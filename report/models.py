651c36ca0074ecab650f627f0ce0770ffa0f89a9
showppark12 <showppark12@naver.com>
2020-08-20 22:07:43 +0900

    리포트 모델 작성

--- report/models.py
+++ report/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from account.models import Account
 
-# Create your models here.
+class Report(models.Model):
+    user = models.ForeignKey(Account, related_name='survivor', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    message = models.charField(max_length=100, blank=True)
+
+    class Meta:
+        db_table ='report'
