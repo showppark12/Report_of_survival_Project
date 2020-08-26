b4cc51e846df045208111214d491efc6b5a56529
박종민 <bagjongmin@bagjongmin-ui-MacBookPro.local>
2020-08-26 18:22:42 +0900

    장고 채팅생성 채팅방 디비저장 채팅 메세지 디비저장

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ChannelName, ChatRoom, Chat_Message
+
+admin.site.register(ChannelName)
+admin.site.register(ChatRoom)
+admin.site.register(Chat_Message)
--- mysite/settings.py
+++ mysite/settings.py
@@ -140,11 +140,11 @@ CORS_ALLOW_CREDENTIALS = True
 #channels
 ASGI_APPLICATION = 'mysite.routing.application'
 
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             "hosts": [('127.0.0.1', 6379)],
-#         },
-#     },
-# }
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
